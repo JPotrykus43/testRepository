24ac639eb15746939c1a5d976e6c9abb264647fd
JPotrykus43 <90198802+JPotrykus43@users.noreply.github.com>
2021-09-20 17:16:15 +0200

    Add files via upload

---
+++ Zadanie20.09.2021.php
@@ -0,0 +1,57 @@
+<?php
+    /*$a = 6;
+    echo gettype($a)."<br>";
+    $a  = 123.5;
+    echo gettype($a)."<br>";
+
+    $t = [1 => "wartosc1", 2 => "element_drugi", 3 => "wartosc3"];
+    echo var_dump($t)."<br>";
+
+    if(is_array($t))
+    {
+        echo "true";
+
+    }
+    else
+    {
+        echo"false";
+    }*/
+
+    /*$a = 6;
+    define("imie","Jakub");
+    define("kierunek","Programista"); 
+    echo "Imie: ".imie." Kierunek: ".kierunek;
+    echo $a;
+      */
+      
+      /*
+      //ZADANIE 1
+      define("kurs",4.58);
+      $a = 20;
+      echo "wedlug kursu 20 eruo to ".kurs*$a." zł"; 
+      
+      */
+      //ZADANIE2
+        /*
+       $x =2;//Przypisuję do x wartość równą 2
+       $y=3;//Przypisuję do y wartość równą 3
+       $z=5;//Przypisuję do z wartość równą 5
+       $x++;//Inkrementuję wartość x o jeden czyli x będzie wynosić 3
+       $y--;//Dekrementuję wartość y o jeden czyli y będzie wynosić 2
+       $z+=$x;//Przypisuję wartość x do wartości z czyli z będzie wynosić 8
+       --$z;//Dekrementuję wartość z o jeden czyli z będzie wynosić 7
+        */
+      /*
+      //ZADANIE3
+      $a = 4;
+      $b = 5;
+      echo "Róźnica: ".$a-$b." Iloraz ".$a/$b. " Modulo ".$a%$b;
+      */
+
+      /*
+      //ZADANIE4
+      $x=7;
+      $y=3;
+      echo ($x+2*$y)/2;
+      */
+ ?>
