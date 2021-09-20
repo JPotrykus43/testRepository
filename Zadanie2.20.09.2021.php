e96478bd2f83cc617b76afe95c0b1160ba7ac550
JPotrykus43 <90198802+JPotrykus43@users.noreply.github.com>
2021-09-20 17:15:45 +0200

    Add files via upload

---
+++ Zadanie2.20.09.2021.php
@@ -0,0 +1,11 @@
+<?php
+//ZADANIE2
+        
+       $x =2;//Przypisuję do x wartość równą 2
+       $y=3;//Przypisuję do y wartość równą 3
+       $z=5;//Przypisuję do z wartość równą 5
+       $x++;//Inkrementuję wartość x o jeden czyli x będzie wynosić 3
+       $y--;//Dekrementuję wartość y o jeden czyli y będzie wynosić 2
+       $z+=$x;//Przypisuję wartość x do wartości z czyli z będzie wynosić 8
+       --$z;//Dekrementuję wartość z o jeden czyli z będzie wynosić 7
+        ?>
