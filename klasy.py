dc1627226961106835dad69fe4db0c1c89af08fe
JPotrykus43 <90198802+JPotrykus43@users.noreply.github.com>
2021-09-29 10:27:00 +0200

    Add files via upload

---
+++ klasy.py
@@ -0,0 +1,9 @@
+punkty = int(input("Podaj liczbę punktów"));
+frekwencja = int(input("Podaj frekewencje"));
+srednia = int(input("Podaj srednia"));
+x=0;
+if (frekwencja>94 and srednia >4):
+    x = punkty +20;
+    print("Twoje punkty teraz", x);
+else:
+        print("Nie ma dodatkowych punktów");
